7e245ae1a6d91df0da3266ad320be60ee2e9c387
Joey <joey@p200300eac7077d299dcd8be14e91cb79.dip0.t-ipconnect.de>
2025-12-28 13:32:10 +0100

    Add the operate function

--- index.js
+++ index.js
@@ -13,3 +13,29 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
 	return num1/num2;
 }
+
+let num1 = 0;
+let num2 = 0;
+let op = "";
+const PLUS = "+";
+const MINUS = "-";
+const DIV = "/";
+const MUL = "*";
+
+function operate(num1, num2, operator){
+	switch (operator) {
+		case PLUS:
+			return num1+num2;
+		case MINUS:
+			return num1-num2;
+		case DIV:
+			return num1/num2;
+		case MUL:
+			return num1*num2;
+		default:
+			console.error("Unknown operator: ", operator);
+
+	}
+}
+
+
